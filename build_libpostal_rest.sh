9efaf3287375bd37e9a46ad7da80d9bdd1319771
Fabrice <archpanda@valendia.fr>
2022-03-14 11:14:40 +0100

    Update build_libpostal_rest.sh

--- build_libpostal_rest.sh
+++ build_libpostal_rest.sh
@@ -11,6 +11,6 @@ export GOROOT=/libpostal/go
 export GOPATH=/libpostal/workspace
 export PATH=$PATH:/libpostal/go/bin
 
-go get github.com/johnlonganecker/libpostal-rest
+go get https://github.com/Archpanda/libpostal-rest
 
-go install github.com/johnlonganecker/libpostal-rest
+go install https://github.com/Archpanda/libpostal-rest
